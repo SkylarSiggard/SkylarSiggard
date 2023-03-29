b071065b79528ae82fecdeae330324a2932cc505
SkylarSiggard <siggardskylar@gmail.com>
2023-03-29 13:19:44 -0600

    done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -24,7 +24,7 @@ export default function Profile() {
             <section>
                 <div className="container">
                     <div className="bio-box">
-                        <p>Meet our talented and ambitious intern developer! With a passion for technology and an appetite for continuous learning, this skilled professional is making waves in the computer software industry. Currently working with Typescript, they bring a wealth of experience in full-stack development, Python, React, HTML, CSS, and JavaScript.
+                        <p>With a passion for technology and an appetite for continuous learning, this skilled professional is making waves in the computer software industry. Currently working with Typescript, they bring a wealth of experience in full-stack development, Python, React, HTML, CSS, and JavaScript.
                             After graduating from Utah Valley University with a bachelor's degree in Criminal Justice, they embarked on a career in tech, earning a Full-Stack Web Development certificate from DevMountain. Alongside their academic pursuits, they were also actively involved in the Cybersecurity Club and CTF Cybersecurity Team.
                             Always eager to learn more and stay ahead of the curve, they have decided to pursue a Masters of Cybersecurity at UVU, starting in August 2023. In preparation for the program, they have already taken prerequisite and refresher courses, demonstrating their dedication to excellence and their commitment to mastering their craft.
                             With a demonstrated history of success and a drive to always do better, our intern developer is a force to be reckoned with in the tech world. Keep an eye out for their future achievements!</p>
