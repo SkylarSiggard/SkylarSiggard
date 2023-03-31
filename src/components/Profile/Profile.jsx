ed2a2be80d7111df58ca4ed386971d90983bc81f
SkylarSiggard <siggardskylar@gmail.com>
2023-03-31 16:26:44 -0600

    done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -24,7 +24,7 @@ export default function Profile() {
             <section>
                 <div className="container">
                     <div className="bio-box">
-                        <p>With a passion for technology and an appetite for continuous learning, this skilled professional is making waves in the computer software industry. Currently working with Typescript, Skylar bring a wealth of experience in full-stack development, Python, React, HTML, CSS, and JavaScript.
+                        <p>With a passion for technology and an appetite for continuous learning, this skilled professional is making waves in the computer software industry. Currently working with Typescript, Skylar brings a wealth of experience in full-stack development, Python, React, HTML, CSS, and JavaScript.
                             After graduating from Utah Valley University with a bachelor's degree in Criminal Justice, Skylar embarked on a career in tech, earning a Full-Stack Web Development certificate from DevMountain. Alongside their academic pursuits, Skylar is also actively involved in the Cybersecurity Club and CTF Cybersecurity Team.
                             Always eager to learn more and stay ahead of the curve, Skylar have decided to pursue a Masters of Cybersecurity at UVU, starting in August 2023. In preparation for the program, Skylar has already taken prerequisite and refresher courses, demonstrating his dedication to excellence and his commitment to mastering his craft.
                             With a demonstrated history of success and a drive to always do better, Skylar is a force to be reckoned with in the tech world. Keep an eye out for his future achievements!</p>
